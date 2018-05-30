3e462fa1c690ee24bf06521d45084968f8f05f38
ndevileye <ndevileyee@gmail.com>
2018-05-30 04:41:42 -0700

    first commit

---
+++ articles/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Article
+from django.contrib.auth.decorators import login_required
+
+def article_list(request):
+    articles = Article.objects.all().order_by('date');
+    return render(request, 'articles/article_list.html', { 'articles': articles })
+
+def article_detail(request, slug):
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
